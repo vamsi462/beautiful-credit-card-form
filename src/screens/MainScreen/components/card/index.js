fa896e65c1684898fa8cc59abee5496815b0e365
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-10 15:43:09 +0530

    styling the card component

--- src/screens/MainScreen/components/card/index.js
+++ src/screens/MainScreen/components/card/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {TransitionGroup, SwitchTransition, CSSTransition } from 'react-transition-group'
+import img from '../../images/card.jpeg' 
+import './styles.scss'
 
-
-const Card= props => {
+const Card= () => {
     return (
-        <div>
-            Card
+        <div className="card-item">
+            <div className="card-item__side">
+                <div className="card-item__focus"/>
+                    <div className="card-item__cover">
+                        <img alt="bg" src={img} className="card-item__bg"/>
+                    </div>
+            </div>
+
         </div>
-    )
+    )   
 }
 
 
