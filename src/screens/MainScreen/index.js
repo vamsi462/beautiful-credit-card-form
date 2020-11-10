031878d3d68c82189716ba3219c2bbfbda8cefc9
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-10 22:48:12 +0530

    inital state values

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CForm from './components/form'
 import Card from './components/card'
 
+const initialState = {
+  cardNumber:"#### #### #### ####",
+  cardName:"FULL NAME",
+  cardMonth:"",
+  cardYear: '',
+  cardCvv: '',
+  isCardFlipped: false
+}
+
 const MainScreen = () => {
+    const [state,setState] = useState(initialState)
+    const [currentFocusedForm,setCurrentFocusedForm]= useState(null)
+
     return (
       <div>
         <Card />
