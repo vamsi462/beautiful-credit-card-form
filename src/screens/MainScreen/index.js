731211618f4dba9c0a584fbb2fb15d9655a0c3bb
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-11 10:17:22 +0530

    references for corresponding input fileds

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import CForm from './components/form'
 import Card from './components/card'
 
@@ -14,6 +14,23 @@ const initialState = {
 const MainScreen = () => {
     const [state,setState] = useState(initialState)
     const [currentFocusedForm,setCurrentFocusedForm]= useState(null)
+
+    const updateStateValues = (keyName,value)=>{
+      setState({
+        ...state,
+        [keyName]:value || initialState[keyName]
+      })
+    }
+
+    //form input references used to focus on corresponding input fields
+    let formFieldsRefObj = {
+      cardNumber: useRef(),
+      cardHolder: useRef(),
+      cardDate: useRef(),
+      cardCvv: useRef(),
+    };
+
+  
 
     return (
       <div>
